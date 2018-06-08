2e975ac27b7a0dffe60fe07bfe0858000792ac14
wesley vieira de lima <wesley.vieira.lima.1999@gmail.com>
2018-06-08 01:12:45 -0300

    telas css refeitas, mapa em nuvem adicionado, async concertado

--- platforms/android/app/src/main/assets/www/js/index.js
+++ platforms/android/app/src/main/assets/www/js/index.js
@@ -1,6 +1,4 @@
-var app = {	
-	mbLatitude: 37.422359,
-	mbLongitude: -122.084344,
+var app = {
     // Application Constructor
     initialize: function() {
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
@@ -10,14 +8,13 @@ var app = {
     //
     // Bind any cordova events here. Common events are:
     // 'pause', 'resume', etc.
-    onDeviceReady: function() {		
-		//this.geolocationLoad();
-		document.getElementById('btnRegisterUser').addEventListener('click', this.dbRegisterUser, false);
-		document.getElementById('btnGoToRegister').addEventListener('click', this.goToRegister, false);
+    onDeviceReady: function() {
+        this.getLatLong();
     },
 	
-	goToRegister: function(){
-		$.mobile.changePage("#pageRegister");
+	goToPage: function(page){
+        if(page) $.mobile.changePage("#"+page);
+        else alert('##página invalida: ' + page);
 	},
 	
 	dbRegisterUser: function(e){	
@@ -26,9 +23,8 @@ var app = {
 		var vEmail = document.getElementById('registerEmail').value;
 		var vTelefone= document.getElementById('registerTelefone').value;
 		var vSenha = document.getElementById('registerSenha').value;
-		if(vNome==""||vEmail==""||vTelefone==""||vSenha==""){ 
-			//window.plugins.toast.show('todos os campos devem estar','long', 'center');
-			alert('todos os campos devem estar preenchidos');
+		if(vNome==""||vEmail==""||vTelefone==""||vSenha==""){
+			alert('##todos os campos devem estar preenchidos');
 		}
 		else{
 			$.ajax({
@@ -40,8 +36,7 @@ var app = {
 					email: vEmail,
 					telefone: vTelefone,
 					senha: vSenha
-				},     
-				async: false,	//perguntar pq quando tá true o php não retorna valor
+				},
 				dataType: "json", 
 				success: function (json) {
 					tttt = json;
@@ -59,59 +54,25 @@ var app = {
 		}
 	},
 	
-	conPhp: function() {
-		$.ajax({
-            type: "POST",
-            url: "http://192.168.0.100/index.php", 
-            data: {
-                acao: 'login'
-            },            
-            async: true,
-            dataType: "json", 
-            success: function (json) { 
-                if(json[0].usuario != ""){
-                   //redireciona o usuario para pagina
-					alert("logado: "+json[0].usuario); 
-                }
-				
-            }
+    getLatLong:function() {
+        var lat = 51.49575692748839;
+        var lon = -0.14600197187496633;
+        navigator.geolocation.getCurrentPosition(function(position) {
+            this.lat = position.coords.latitude;
+            this.lon = position.coords.longitude;
+            console.log(this.lat);
+            app.loadMap(this.lat, this.lon);
         });
-	},
-	
-	geolocationLoad:function(position) {
-		navigator.geolocation.getCurrentPosition(app.onGeolocationCurrentPositionSuccess);
-	},
-	
-    onGeolocationCurrentPositionSuccess:function(position){
-        var a = this.mbLatitude;
-		var b = this.mbLongitude;
-		a = position.coords.latitude;
-		b = position.coords.longitude;
-		try{
-			app.loadMap(a, b);
-		}catch(err){alert(err);}
     },
-	
-    loadMap:function(a, b) {
-		var map;
-		var div;
-		var myOptions;
-		myOptions = {
-			zoom: 8
-		};
-		div = document.getElementById("pageMap");
-		map = plugin.google.maps.Map.getMap(div);
-		map.animateCamera({
-			target: {lat: a, lng: b},
-			zoom: 17,
-			bearing: 140,
-			duration: 5000
-		});	/*
-		var marker = map.addMarker({
-			position: {lat: a, lng: b},
-			title:  "Você está aqui",
-			animation: plugin.google.maps.Animation.BOUNCE
-		});		*/
+
+    loadMap:function (lat, lon) {
+        var div;
+        var map;
+        div = document.getElementById("theMap");
+        map = new google.maps.Map(div, {
+            center: {lat: lat, lng: lon},
+            zoom: 15    //quanto maior mais proximo
+        });
     }
 };
 app.initialize();
